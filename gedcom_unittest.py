8d4b130b1b00d1463331643d6cb417e980cbd954
Stevenl1221 <steven.huu.ly@gmail.com>
2022-06-20 22:27:05 -0400

    added user story 13 and 14 for sprint 2

--- gedcom_unittest.py
+++ gedcom_unittest.py
@@ -197,8 +197,45 @@ class TestGedcomFile(unittest.TestCase):
 
 
     #User story 13 - Birth dates of siblings should be more than 8 months apart or less than 2 days apart (twins may be born one day apart, e.g. 11:59 PM and 12:02 AM the following calendar day)
-
+    def test_us_13(self):
+        for family in table[1]:
+            for indiv in table[0]:
+                if indiv.id in family.children:
+                    if len(family.children) >= 2:
+                        for child in family.children:
+                            for i in table[0]:
+                                if (child==i.id):
+                                    temp_birth = indiv.birth.split(" ")
+                                    indiv_birth = datetime.datetime(int(temp_birth[2]), month_dict[temp_birth[1]], int(temp_birth[0]))
+                                    temp_child = i.birth.split(" ")
+                                    child_birth = datetime.datetime(int(temp_child[2]), month_dict[temp_child[1]], int(temp_child[0]))
+                                    check_birth = abs((indiv_birth.year - child_birth.year)*12 + (indiv_birth.month - child_birth.month)) > 8 or abs( (indiv_birth - child_birth).days ) < 2
+                                    if not check_birth:
+                                        print("Children should be born more than 8 months apart or less than 2 days apart")
+        print('Test 13 passed succesfully')
     #User story 14 - No more than five siblings should be born at the same time
+    def test_us_14(self):
+        for family in table[1]:
+            for indiv in table[0]:
+                count = 0
+                if indiv.id in family.children:
+                    if len(family.children) > 5:
+                        for child in family.children:
+                            for i in table[0]:
+                                if (child==i.id):
+                                    temp_birth = indiv.birth.split(" ")
+                                    indiv_birth = datetime.datetime(int(temp_birth[2]), month_dict[temp_birth[1]], int(temp_birth[0]))
+                                    temp_child = i.birth.split(" ")
+                                    child_birth = datetime.datetime(int(temp_child[2]), month_dict[temp_child[1]], int(temp_child[0]))
+                                    check_birth = abs( (indiv_birth - child_birth).days ) < 2
+                                    if check_birth:
+                                        count+=1
+                                        if count>5:
+                                            print("No more than five siblings should be born at the same time")
+                    else:
+                        break
+        print('Test 14 passed succesfully')
+
 
 if __name__ == '__main__':
     unittest.main()
